2f98eeb049f320abb597c34ee306eccc2a173c47
Niket Khandelwal <niketkh@gmail.com>
2016-05-15 00:46:50 +0000

    display errors, show template and action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,13 +6,17 @@ class ArticlesController < ApplicationController
     def create
         # render plain: params[:article].inspect
         @article = Article.new(article_params)
-        if @aricle.save
+        if @article.save
             flash[:notice] = "Article was successfully created"
             redirect_to article_path(@article)
         else
-            # render 'new'
-            render :new
+            render 'new' 
+            # render :new
         end
+    end
+    
+    def show
+            @article = Article.find(params[:id])
     end
     
     
